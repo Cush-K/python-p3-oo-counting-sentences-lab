7263c5973786b5f784fd2993d5c5fff1c9c260f4
Kalvine <kkalvine4@gmail.com>
2024-08-31 15:27:58 +0300

    Uses Regular Expressions to count sentences

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,39 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+      self._value = None
+      self.value = value
+     
+
+    @property
+    def value(self):
+       return self._value
+
+    @value.setter
+    def value(self, value):
+       if isinstance(value, str):
+          self._value = value
+       else:
+          print("The value must be a string.")
+
+    def is_sentence(self):
+      return self._value.endswith('.') if self._value else False
+               
+    def is_question(self):
+       return self._value.endswith('?') if self._value else False
+       
+    def is_exclamation(self):
+       return self._value.endswith('!') if self._value else False
+       
+    def count_sentences(self):
+        if isinstance(self._value, str):
+
+          import re
+          sentences = re.split('[.!?]', self._value)
+
+          sentences = [sentence for sentence in sentences if sentence]
+          return len(sentences)
+        else:
+           print("The value must be a string.")
+           return 0
